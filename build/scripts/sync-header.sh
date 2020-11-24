11ce25239005e07472481e8bc84243508fa49226
Brian Cassidy <brian.cassidy@unb.ca>
2020-11-24 15:07:14 -0400

    PRESERVE-11 convert template to use unb_lib_theme bs4

--- build/scripts/sync-header.sh
+++ build/scripts/sync-header.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl -Ls https://lib.unb.ca/core/inc-2015/head-nav.php > /app/templates/head-nav.html
+curl -Ls https://github.com/unb-libraries/unb_lib_theme/raw/8.x-bootstrap-4.x/templates/includes/navbar.html.twig > /app/templates/navbar.html.twig
