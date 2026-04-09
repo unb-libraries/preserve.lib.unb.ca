d42d2a58136cfefdc52fb9d67d0d1320e130a609
Brian Cassidy <brian.cassidy@unb.ca>
2026-04-09 12:25:55 -0300

    update menu and update location

--- build/scripts/sync-header.sh
+++ build/scripts/sync-header.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-curl -Ls https://raw.githubusercontent.com/unb-libraries/unb_lib_theme/8.x-4.x/templates/includes/navbar.html.twig > /app/templates/navbar.html.twig
+curl -Ls https://raw.githubusercontent.com/unb-libraries/unb_lib_theme/10.x-4.x/templates/includes/navbar.html.twig > /app/templates/navbar.html.twig
